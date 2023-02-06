38573607f03f2404aa2b37c926b4f37abdc42ef0
Will Mills <will.mills@shopify.com>
2023-02-06 17:24:45 -0500

    Use cargo for project management

---
+++ hello_cargo/target/debug/deps/hello_cargo-ba89cd09eae31629.d
@@ -0,0 +1,5 @@
+/Users/willmills/src/github.com/W-Mills/rust/hello_cargo/target/debug/deps/hello_cargo-ba89cd09eae31629: src/main.rs
+
+/Users/willmills/src/github.com/W-Mills/rust/hello_cargo/target/debug/deps/hello_cargo-ba89cd09eae31629.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/willmills/src/github.com/W-Mills/rust/hello_cargo/target/debug/hello_cargo: /Users/willmills/src/github.com/W-Mills/rust/hello_cargo/src/main.rs
